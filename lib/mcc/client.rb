5479114065157708cfa78ad0ac6aeae84188d574
Ryan Doyle <ryan@doylenet.net>
2013-07-01 22:01:11 +1000

    reformat tabs to spaces

--- lib/mcc/client.rb
+++ lib/mcc/client.rb
@@ -1,40 +1,40 @@
 require 'xmlrpc/client'
 
 module MCC
-	class Client
-	
-		def initialize(server)
-			begin
-				host = server.split(":")
-				@rpc = XMLRPC::Client.new(host[0], "/", host[1])
-			rescue
-				raise "MCC_SERVER environment variable not set"
-			end
-		end
-		
-		def get_instances(opts = {})
-			res = @rpc.call("openvz.get_instances", opts)
-			return res
-		end
-		
-		def get_images()
-			res = @rpc.call("openvz.get_images")
-			return res
-		end
-		
-		def get_instance_types()
-			res = @rpc.call("openvz.get_instance_types")
-			return res
-		end
-		
-		def run_instance(template, type, pubkey, name)
-			res = @rpc.call("openvz.run_instance", template, type, pubkey, name)
-			return res
-		end
-		
-		def destroy_instance(id)
-			res = @rpc.call("openvz.destroy_instance", id)
-			return res
-		end  
-	end
+  class Client
+  
+    def initialize(server)
+      begin
+        host = server.split(":")
+        @rpc = XMLRPC::Client.new(host[0], "/", host[1])
+      rescue
+        raise "MCC_SERVER environment variable not set"
+      end
+    end
+    
+    def get_instances(opts = {})
+      res = @rpc.call("openvz.get_instances", opts)
+      return res
+    end
+    
+    def get_images()
+      res = @rpc.call("openvz.get_images")
+      return res
+    end
+    
+    def get_instance_types()
+      res = @rpc.call("openvz.get_instance_types")
+      return res
+    end
+    
+    def run_instance(template, type, pubkey, name)
+      res = @rpc.call("openvz.run_instance", template, type, pubkey, name)
+      return res
+    end
+    
+    def destroy_instance(id)
+      res = @rpc.call("openvz.destroy_instance", id)
+      return res
+    end  
+  end
 end
--- lib/mcc/server.rb
+++ lib/mcc/server.rb
@@ -1,22 +1,22 @@
 require 'xmlrpc/server'
 module MCC
-	class Server
-	
-		def initialize(port, listen, iprange, log)
-			@port = port
-			@listen = listen
-			@iprange = iprange
-			@logtype = log
-			$ip_store = Array.new
-			$ctid_store = Array.new
-		end
-		
-		def run()
-			s = XMLRPC::Server.new(@port, @listen, 10, @logtype)
-			s.add_handler("openvz", MCC::OpenVZ.new(@iprange, @logtype))
-			s.serve
-		end
-	
-	end
+  class Server
+  
+    def initialize(port, listen, iprange, log)
+      @port = port
+      @listen = listen
+      @iprange = iprange
+      @logtype = log
+      $ip_store = Array.new
+      $ctid_store = Array.new
+    end
+    
+    def run()
+      s = XMLRPC::Server.new(@port, @listen, 10, @logtype)
+      s.add_handler("openvz", MCC::OpenVZ.new(@iprange, @logtype))
+      s.serve
+    end
+  
+  end
 end
 

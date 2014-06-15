882c8a78ffea7967f44b62ac4e64b317f68e3593
Ryan Doyle <ryan@doylenet.net>
2014-06-15 12:18:03 +1000

    move constant into constructor

--- lib/mcc/keystore.rb
+++ lib/mcc/keystore.rb
@@ -1,40 +1,40 @@
 module MCC
   class Keystore
-    
-    KS = File.expand_path('~/.mcc/keys/') + '/'
-    
+
+
     def initialize()
+      @keystore_root = File.expand_path('~/.mcc/keys/') + '/'
       # Check if the .mcc/keys directory exists and create it
-      if ! File.directory?(KS)
+      if ! File.directory?(@keystore_root)
         # Can't find a way to make the parent
         Dir.mkdir(File.expand_path('~/.mcc'), 0700)
-        Dir.mkdir(KS, 0700)
+        Dir.mkdir(@keystore_root, 0700)
       end
     end
     
     def add_key(keyname, pub)
       raise "Key " + keyname + " already exists" if File.file?(KS + keyname + ".pub")
-      f = File.open(KS + keyname + ".pub", 'w+', 0600)
+      f = File.open(@keystore_root + keyname + ".pub", 'w+', 0600)
       f.write(pub + "\n")
       f.close
     end
     
     def del_key(keyname)
-      File.delete(KS + keyname + ".pub")
+      File.delete(@keystore_root + keyname + ".pub")
     end
     
     def get_pub_key(keyname)
-      File.read(KS + keyname + ".pub")
+      File.read(@keystore_root + keyname + ".pub")
     end
     
     def get_keys()
-      Dir.foreach(KS).map do |f|
+      Dir.foreach(@keystore_root).map do |f|
         f.gsub('.pub', '') if f.include?('.pub')
       end.compact
     end
     
     def exists?(keyname)
-      return File.file?(KS + keyname + ".pub")
+      return File.file?(@keystore_root + keyname + ".pub")
     end
   
   end

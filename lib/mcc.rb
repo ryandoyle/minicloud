b0163c0f09f2de0295878ef499c88f3f2c192aab
Ryan Doyle <ryan@doylenet.net>
2014-06-29 14:40:53 +1000

    Remove really ugly require statements

--- lib/mcc.rb
+++ lib/mcc.rb
@@ -1,17 +1,3 @@
 module MCC
-  # Source the client and server libs
-  libs = ["client", "server", "openvz", "keystore"]
-  libs.each do |lib|
-    require File.expand_path(
-      File.join(
-        File.dirname(
-          File.expand_path(
-            File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-          )
-        ), "mcc/"+lib
-      )
-    )
-  
-  end
 end
 
